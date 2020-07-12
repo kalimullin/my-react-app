a5695be035ff7828468c4f35b11959d47f277b25
Ilnur Kalimullin <ilnur@klmln.ru>
2020-07-12 17:48:45 +0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,9 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Main" />
+    <h1>Learn to design and code React apps</h1>
+    <p>Complete courses</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
