b36a9cad8e47273c26aec07df272adffdd94e985
Ilnur Kalimullin <ilnur@klmln.ru>
2020-07-12 17:58:01 +0300

    Test commit...

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Main" />
     <h1>Learn to design and code React apps</h1>
-    <p>Test message...</p>
+    <p>Another text message...</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
